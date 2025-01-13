7ef39057c2de68a443e009f4fb8c9009adf646db
Aashish70 <ashishsingh70002@gmail.com>
2025-01-13 10:01:17 +0530

    Set up React project with Vite and Tailwind CSS, including initial configuration files and components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import HeaderSection from "./components/dashboard/HeaderSection";
+
+
+function App() {
+  
+
+  return (
+    <>
+      <HeaderSection />
+    </>
+  )
+}
+
+export default App
